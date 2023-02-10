3e0bb0b22fc5b0653bef1b39fa76e7edf9094269
Min-214 <113558117+Min-214@users.noreply.github.com>
2023-02-10 23:37:09 +0300

    Update tasks4.py

--- tasks4.py
+++ tasks4.py
@@ -4,8 +4,6 @@ def leap_year(year):
     return year % 4 == 0 and year % 100 != 0 or year % 400 == 0
 
 
-# print(leap_year(1804))
-
 def penny(pen):
 
     pen_str = str(pen)
@@ -27,3 +25,30 @@ def penny(pen):
 
     if int(pen_str[-1]) == 1:
         return f'{pen} - копейка'
+    
+
+def zodiac(d, m):
+    if m == 3 and 21 <= d <= 31 or m == 4 and 1 <= d <= 20:
+        return 'Овен'
+    elif m == 4 and 21 <= d <= 30 or m == 5 and 1 <= d <= 20:
+        return 'Телец'
+    elif m == 5 and 21 <= d <= 31 or m == 6 and 1 <= d <= 21:
+        return 'Близнецы'
+    elif m == 6 and 22 <= d <= 30 or m == 7 and 1 <= d <= 22:
+        return 'Рак'
+    elif m == 7 and 23 <= d <= 31 or m == 8 and 1 <= d <= 23:
+        return 'Лев'
+    elif m == 8 and 24 <= d <= 31 or m == 9 and 1 <= d <= 23:
+        return 'Дева'
+    elif m == 9 and 24 <= d <= 30 or m == 10 and 1 <= d <= 23:
+        return 'Весы'
+    elif m == 10 and 24 <= d <= 31 or m == 11 and 1 <= d <= 22:
+        return 'Скорпион'
+    elif m == 11 and 23 <= d <= 30 or m == 12 and 1 <= d <= 21:
+        return 'Стрелец'
+    elif m == 12 and 22 <= d <= 31 or m == 1 and 1 <= d <= 20:
+        return 'Козерог'
+    elif m == 1 and 21 <= d <= 31 or m == 2 and 1 <= d <= 18:
+        return 'Водолей'
+    elif m == 2 and 19 <= d <= 29 or m == 3 and 1 <= d <= 20:
+        return 'Рыбы'
